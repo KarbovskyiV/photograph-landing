ce789f0a236b2b748f0e00e67dbf590b220a5e72
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-10-03 14:02:01 +0300

    add header

---
+++ src/components/Headers/BaseHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="h-2-container">
+    <DecorLeft />
+    <span>{{ name }}</span>
+    <DecorRight />
+  </div>
+</template>
+
+<script setup>
+import DecorLeft from '@/components/icons/DecorLeft.vue'
+import DecorRight from '@/components/icons/DecorRight.vue'
+
+defineProps({
+  name: String
+})
+</script>
+
+<style scoped>
+.h-2-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+span {
+  font-size: 40px;
+  font-weight: 600;
+  line-height: 60px;
+}
+</style>
